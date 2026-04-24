992231341e11fbff3fb25a137e25edb1c1a39f74
devmindtan <devmind.tan@gmail.com>
2026-04-24 10:18:35 +0000

    Manual backup: 2026-04-24 10:18:35

--- Configurations/tools/zsh/.oh-my-zsh/custom/fzf.zsh
+++ Configurations/tools/zsh/.oh-my-zsh/custom/fzf.zsh
@@ -10,7 +10,7 @@ blue="#06BCE4"
 cyan="#2CF9ED"
 
 export FZF_DEFAULT_OPTS="--color=fg:${fg},bg:${bg},hl:${purple},fg+:${fg},bg+:${bg_highlight},hl+:${purple},info:${blue},prompt:${cyan},pointer:${cyan},marker:${cyan},spinner:${cyan},header:${cyan}"
-export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
+export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git --exclude '*.zwc'"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
 _fzf_compgen_path() {
@@ -22,7 +22,7 @@ _fzf_compgen_dir() {
   fd --type=d --hidden --exclude .git . "$1"
 }
 
-source ./fzf-git.sh
+# source $ZSH_CUSTOM/fzf-git.zsh
 
 show_file_or_dir_preview="if [ -d {} ]; then eza --tree --color=always {} | head -200; else bat -n --color=always --line-range :500 {}; fi"
 
